c77636e1e20541c39d74cd3e264d22c108501b35
Richard V.H <richardv@theluxergroup.com>
2025-09-17 10:12:12 -0700

    added tmux navigater

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,5 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-vim.wo.relativenumber = true
+vim.o.relativenumber = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
 return {}
